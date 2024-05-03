978b367bfcc15d8f1a0a174aa8e6735e42b9b06f
ahmed elgendy <ahmed.elgendy@innovitics.com>
2024-05-03 13:55:41 +0300

    removing status bar color

--- app/src/main/java/com/gendy/bugIt/activities/MainActivity.kt
+++ app/src/main/java/com/gendy/bugIt/activities/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gendy.bugIt.ui.theme.BugItTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,6 +21,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+        installSplashScreen()
 
         enableEdgeToEdge()
 
